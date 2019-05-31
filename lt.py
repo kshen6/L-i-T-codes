59b233cda2cd8a0160d1b721ba4e896177d3f6f0
Kendrick Shen <kshen6@stanford.edu>
2019-05-31 01:06:38 -0700

    more changes

--- lt.py
+++ lt.py
@@ -11,36 +11,60 @@ class Packet():
     :param data - store the xor'd information
     :param seed - used for recovering indices
     '''
-    def __init__(self, data, degree, seed):
+    def __init__(self, data, degree, num_blocks, seed):
         self.data = data
         self.degree = degree
+        self.num_blocks = num_blocks
         self.seed = seed
     
-    def recover_source(self, num_blocks):
+    def recover_source(self):
         '''
         Using degree and random seed, recover the indices of blocks that
         were xor'd together to generate data
         '''
         random.seed(self.seed)
-        indices = random.sample(list(range(1, num_blocks + 1)), self.degree)
+        indices = random.sample(list(range(1, self.num_blocks + 1)), self.degree)
         self.indices = indices
 
 class Encoder():
-    def __init__(self, data, soliton):
+    def create_blocks(self, data, blk_sz, pad_with=0):
+        '''
+        divide data into blocks of tunable size
+        :param blk_sz - desired block size
+        :param pad_with - if size of data is not evenly divisible, ensure that all
+            blocks are of the same size
+        '''
         self.data = data
-        self.soliton = soliton
-    
-    def create_blocks(self, block_size):
-        blocks = np.array_split(self.data, len(self.data) // block_size)
+        blocks = [self.data[i:i + blk_sz] for i in range(0, len(self.data), blk_sz)]
+        blocks[-1] += [0] * (blk_sz - len(blocks[-1]))
         self.blocks = blocks
     
-    def encode(self):
-        num = len(self.blocks)
-        indices = np.random.choice(list(range(1, num + 1)), num, p=self.soliton(num))
-        res = self.blocks[indices[0]]
-        for i in range(1, len(indices)):
-            res ^= self.blocks[indices[i]]
-        yield Packet(res, indices)
+    def encode(self, seed, soliton, num_to_transmit = float('inf')):
+        '''
+        generator function to indefinitely produce xor'd Packets
+        :param seed - random seed for recovering indices
+        :param soliton - desired degree distribution
+        must call create_blocks before calling this function
+        '''
+        assert hasattr(self, 'blocks'), 'Must call create_blocks to initialize'
+
+        counter = 0
+        while counter < num_to_transmit:
+            num_blocks = len(self.blocks)
+            # choose a degree based on the given soliton distribution
+            degree = np.random.choice(list(range(1, num_blocks + 1)), num_blocks, \
+                p=soliton(num_blocks))
+            # seed so that we can recover the source block indices
+            random.seed(seed)
+            indices = random.sample(list(range(1, num_blocks + 1)), degree)
+            # XOR all blocks
+            block = self.blocks[indices[0]]
+            for b in indices[1:]:
+                block ^= self.blocks[b]
+            yield Packet(block, degree, num_blocks, seed)
+            # do not duplicate seeds
+            seed += 1
+            counter += 1
 
 class Fountain():
     def __init__(self, network):
@@ -50,8 +74,18 @@ class Fountain():
         raise NotImplementedError()
 
 class Decoder():
-    def __init__(self, stream):
-        self.stream = stream
+    def __init__(self):
+        self.received = []
+        self.belief = []
     
-    def decode(self):
-        raise NotImplementedError()
+    def incorporate(self, packet):
+        self.received.append(packet)
+        self.propagate_beliefs()
+    
+    def propagate_beliefs(self):
+        pass
+
+    def decode(self, packets):
+        for p in packets:
+            p.recover_source()
+        self.packets = packets
