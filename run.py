312870651f198d6f52716a8685fed381dc34e47b
seankdecker <seankdecker@gmail.com>
2019-05-31 13:29:34 -0700

    added a superficial noisy channel which we can tune the noise of. Can now intergrate encoding code

--- run.py
+++ run.py
@@ -1,31 +1,57 @@
-from multiprocessing import Process
+from multiprocessing import Process, Pipe
 import time
+import random
 from lt import Packet, Encoder, Decoder
 
-class NoisyPipe():
-    """
-    represents tunable noise in a a noisy channel.
-    This process sits between a sender and receiver
-    probabalistically dropping packets
+# class NoisyPipe():
+#     """
+#     represents tunable noise in a a noisy channel.
+#     This process sits between a sender and receiver
+#     probabalistically dropping packets
+#
+#     piping works as:
+#     external_pipe1 --- internal_pipe1 -- NOISE -- internal_pipe2 -- external_pipe2
+#     """
+#     def __init__(self, noise: float):
+#         self.internal_pipes, self.external_pipes = [None] * 2, [None] * 2
+#         self.internal_pipes[0], self.external_pipes[0] = Pipe()
+#         self.internal_pipes[1], self.external_pipes[1] = Pipe()
+#         self.noise = noise
+#
+#     def changeNoise(self, new_noise: float):
+#         self.noise = new_noise
+#
+#     def getPipeEnd(self, index: int):
+#         return self.external_pipes[index]
+#
+#     def send(self):
+#         packet = self.internal_pipes[0].recv()
+#         sefl.internal_piprs[1].send(packet)
 
-    piping works as:
-    external_pipe1 --- internal_pipe1 -- NOISE -- internal_pipe2 -- external_pipe2
+class NoisyConnection():
     """
-    def __init__(self, noise: float):
-        self.internal_pipes, self.external_pipes = [None] * 2, [None] * 2
-        self.internal_pipes[0], self.external_pipes[0] = Pipe()
-        self.internal_pipes[1], self.external_pipes[1] = Pipe()
+    Defines a connection with a tunable noise factor which defines how likely
+    we are to send a given package. A package is lost with probability noise
+
+    Based off of the Connection class defined in multiprocessing
+    """
+    def __init__(self, connection, noise: float):
+        self.connection = connection
         self.noise = noise
 
     def changeNoise(self, new_noise: float):
         self.noise = new_noise
 
-    def getPipeEnd(self, index: int):
-        return self.external_pipes[index]
+    def send(self, package):
+        if (random.random() > self.noise):
+            self.connection.send(package)
 
-    def send(self):
-        packet = self.internal_pipes[0].recv()
-        sefl.internal_piprs[1].send(packet)
+    def recv(self):
+        return self.connection.recv()
+
+    def close(self):
+        close(self.connection)
+
 
 class Sender():
     """
@@ -33,11 +59,13 @@ class Sender():
     Sender.run is built to be run by a subprocess
     """
     def __init__(self):
-        self.message = "hi from sender"
+        self.message = "hi there fren"
 
-    def run(self):
+    # program run by sender process
+    def run(self, noisyConn):
         while True:
-            print(self.message)
+            print('I\'m sending: ', self.message, ' to my friend')
+            noisyConn.send(self.message)
             time.sleep(2)
 
 
@@ -47,18 +75,28 @@ class Receiver():
     Sender.run is built to be run by a subprocess
     """
     def __init__(self):
-        self.message = "hi from receiver"
+        pass
 
-    def run(self):
+    # program run by receiver process
+    def run(self, noisyConn):
         while True:
-            print(self.message)
-            time.sleep(2)
+            mes = noisyConn.recv()
+            print('ooh, I got a letter! It says: ', mes)
+
 
 
 if __name__ == '__main__':
-    r = Receiver()
-    s = Sender()
-    pr = Process(target=r.run) # receiver process
-    ps = Process(target=s.run) # sender process
+    # Define thta 30% of packages get lost
+    NOISE = 0.3
+
+    # build sender and receiver
+    receiver, sender, = Receiver(), Sender()
+    # build noiseless connections
+    receiverConn, senderConn = Pipe() # from import multiprocessing
+
+    pr = Process(target=receiver.run, args=(NoisyConnection(receiverConn, NOISE),)) # spawn receiver process
+    ps = Process(target=sender.run, args=(NoisyConnection(senderConn, NOISE), )) # spawn sender process
+
+    # begin processes
     pr.start()
     ps.start()
