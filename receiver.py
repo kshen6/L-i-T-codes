a32725dbe97aea5741cbac031dfc3a04fbe6a6e1
seankdecker <seankdecker@gmail.com>
2019-06-01 00:38:56 -0700

    commented sender and receiver and added the ability to specify the use of either tcp or udp

--- receiver.py
+++ receiver.py
@@ -1,13 +1,43 @@
-import socket
+"""
+receiver.py
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
+To create a receiver, run
+    python receiver.py [-proto]
+where -proto can be -tcp or -udp.
+"""
+import socket # for socket
+import sys # for command line arguments
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+# local host IP address listen on
+IP = "127.0.0.1"
+# port we are listening on
+PORT = 5005
+# constants that define what protocol to use
+PROTOS = [socket.SOCK_DGRAM, socket.SOCK_STREAM]
 
-sock.bind((UDP_IP, UDP_PORT))
+def parseArgs():
+    """
+    parse command line to find what protocol to use
+    """
+    if (len(sys.argv) < 2):
+        print('specify the protocol you want to implement')
+        exit(1)
+    if (sys.argv[1] == '-udp'):
+        return 0
+    elif (sys.argv[1] == '-tcp'):
+        return 1
+    else:
+        print('supported protocols include: -udp, -tcp')
+        exit(1)
 
-while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print("received message:", data)
+if __name__ == '__main__':
+    # create socket to listen on
+    sock = socket.socket(socket.AF_INET, # Internet
+                         PROTOS[parseArgs()]) # UDP
+    # bind socket to our IP and PORT
+    sock.bind((IP, PORT))
+
+    # continurally listen and print out message
+    while True:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        print('received message:', data.decode(), 'from ip: ', addr[0], ', port:', addr[1])
--- sender.py
+++ sender.py
@@ -1,14 +1,46 @@
-import socket
+"""
+sender.py
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
+To create a sender, run
+    python sender.py [-proto]
+where -proto can be -tcp or -udp.
 
-print("UDP target IP:", UDP_IP)
-print("UDP target port:", UDP_PORT)
-print("message:", MESSAGE)
+"""
+import socket # for socket
+import sys # for command line arguments
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+# local host IP address to send to, we are sending a message to ourself
+IP = "127.0.0.1"
+# port to send message to
+PORT = 5005
+# constants that define what protocol to use
+PROTOS = [socket.SOCK_DGRAM, socket.SOCK_STREAM]
 
-sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+def createMessage():
+    """
+    returns a message to be send to the receiever
+    """
+    return "Hello World"
+
+def parseArgs():
+    """
+    parse command line to find what protocol to use
+    """
+    if (sys.argv[1] == '-udp'):
+        return 0
+    elif (sys.argv[1] == '-tcp'):
+        return 1
+    else:
+        print('specify the protocol you want to implement as:\n \
+             python sender.py [-udp / -tcp ]')
+
+if __name__ == '__main__':
+    print('targeting IP:', IP, 'target port:', PORT)
+    print('message:', createMessage())
+    proto = parseArgs()
+
+    sock = socket.socket(socket.AF_INET, # Internet
+                         PROTOS[proto]) # Protocol
+
+    # send message to receiver at IP, PORT
+    sock.sendto(createMessage().encode(), (IP, PORT))
